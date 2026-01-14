f4e4d4ee1453d7d719be20e92364d7c7c15f174e
Linermao <LinermaoGemail@gmail.com>
2026-01-14 06:18:20 +0000

    Add the set of ioctls required for Xorg to start successfully on DRM-backed systems.
    Some ioctls are still not fully implemented; however, they now return
    correct and expected error codes to match upstream DRM behavior. This
    is sufficient for user-space probing logic and avoids early aborts.
    
    - Xorg is able to start correctly
    - games and mesa-demos test tools can run successfully

--- kernel/src/syscall/futex.rs
+++ kernel/src/syscall/futex.rs
@@ -137,7 +137,7 @@ pub fn sys_futex(
         }
         _ => {
             warn!("futex op = {:?}", futex_op);
-            return_errno_with_message!(Errno::EINVAL, "unsupported futex op");
+            return_errno_with_message!(Errno::ENOSYS, "unsupported futex op");
         }
     }
     .map_err(|err| match err.error() {
