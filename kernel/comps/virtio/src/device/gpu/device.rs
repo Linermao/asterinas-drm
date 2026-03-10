c58837da6539b50b07f87d1ac95f3a53f06290f5
Wei Zhang <ruoyuan.zw@antgroup.com>
2026-03-10 14:57:03 +0800

    Add VIRTGPU_TRANSFER_FROM_HOST ioctl cmd
    Signed-off-by: Wei Zhang <ruoyuan.zw@antgroup.com>

--- kernel/comps/virtio/src/device/gpu/device.rs
+++ kernel/comps/virtio/src/device/gpu/device.rs
@@ -16,13 +16,13 @@ use ostd::{
 use ostd::prelude::println;
 use super::{CMD_GET_CAPSET, CMD_GET_CAPSET_INFO, CMD_GET_DISPLAY_INFO, CMD_GET_EDID,
     CMD_RESOURCE_ATTACH_BACKING, CMD_RESOURCE_CREATE_2D, CMD_RESOURCE_UNREF, CMD_RESOURCE_DETACH_BACKING, CMD_RESOURCE_FLUSH,
-    CMD_TRANSFER_TO_HOST_2D, CMD_SET_SCANOUT, CMD_SUBMIT_3D, DEVICE_NAME, GpuFeatures, QUEUE_CONTROL,
+    CMD_TRANSFER_FROM_HOST_3D, CMD_TRANSFER_TO_HOST_2D, CMD_SET_SCANOUT, CMD_SUBMIT_3D, DEVICE_NAME, GpuFeatures, QUEUE_CONTROL,
     QUEUE_CURSOR, RESP_OK_CAPSET, RESP_OK_CAPSET_INFO, RESP_OK_DISPLAY_INFO, RESP_OK_EDID, RESP_OK_NODATA,
     VirtioGpuConfig, VirtioGpuCtrlHdr, VirtioGpuDisplayOne, VirtioGpuFormat, VirtioGpuGetCapset, VirtioGpuGetCapsetInfo,
     VirtioGpuGetEdid, VirtioGpuMemEntry, VirtioGpuRect, VirtioGpuResourceAttachBacking,
     VirtioGpuResourceUnref, VirtioGpuResourceDetachBacking,
     VirtioGpuResourceCreate2d, VirtioGpuRespCapsetInfo, VirtioGpuRespDisplayInfo,
-    VirtioGpuRespEdid, VirtioGpuResourceFlush, VirtioGpuTransferToHost2d, VirtioGpuSetScanout, VirtioGpuCmdSubmit,
+    VirtioGpuRespEdid, VirtioGpuResourceFlush, VirtioGpuTransferHost3d, VirtioGpuTransferToHost2d, VirtioGpuSetScanout, VirtioGpuCmdSubmit,
 };
 use crate::{
     device::{VirtioDeviceError, gpu::drm::VirtioGpuDrmDrvier},
@@ -642,6 +642,35 @@ impl VirtioGpuDevice {
         };
         let _: VirtioGpuCtrlHdr = self
             .submit_control_command::<VirtioGpuTransferToHost2d, VirtioGpuCtrlHdr>(
+                &req,
+                RESP_OK_NODATA,
+            )?;
+        Ok(())
+    }
+
+    pub fn transfer_from_host_3d(
+        &self,
+        resource_id: u32,
+        box_: super::VirtioGpuBox,
+        level: u32,
+        offset: u64,
+        stride: u32,
+        layer_stride: u32,
+    ) -> Result<(), VirtioGpuCommandError> {
+        let req = VirtioGpuTransferHost3d {
+            hdr: VirtioGpuCtrlHdr {
+                type_: CMD_TRANSFER_FROM_HOST_3D,
+                ..Default::default()
+            },
+            box_,
+            offset,
+            resource_id,
+            level,
+            stride,
+            layer_stride,
+        };
+        let _: VirtioGpuCtrlHdr = self
+            .submit_control_command::<VirtioGpuTransferHost3d, VirtioGpuCtrlHdr>(
                 &req,
                 RESP_OK_NODATA,
             )?;
--- kernel/src/device/drm/ioctl_defs.rs
+++ kernel/src/device/drm/ioctl_defs.rs
@@ -2,6 +2,7 @@ use aster_gpu::drm::ioctl::*;
 use aster_virtio::device::gpu::drm::{
     VirtioGpuExecbuffer, VirtioGpuGetCaps, VirtioGpuGetParam, VirtioGpuMap,
     VirtioGpuResourceCreate,
+    VirtioGpuTransferFromHost,
     VirtioGpuResourceInfo,
 };
 
@@ -44,6 +45,7 @@ pub(super) type DrmIoctlModeCursor2             = ioc!(DRM_IOCTL_MODE_CURSOR2,  
 pub(super) type DrmIoctlVirtioGpuGetParam       = ioc!(DRM_IOCTL_VIRTGPU_GETPARAM,          b'd', 0x43, InOutData<VirtioGpuGetParam>);
 pub(super) type DrmIoctlVirtioGpuExecbuffer     = ioc!(DRM_IOCTL_VIRTGPU_EXECBUFFER,        b'd', 0x42, InOutData<VirtioGpuExecbuffer>);
 pub(super) type DrmIoctlVirtioGpuResourceCreate = ioc!(DRM_IOCTL_VIRTGPU_RESOURCE_CREATE,   b'd', 0x44, InOutData<VirtioGpuResourceCreate>);
+pub(super) type DrmIoctlVirtioGpuTransferFromHost = ioc!(DRM_IOCTL_VIRTGPU_TRANSFER_FROM_HOST, b'd', 0x46, InOutData<VirtioGpuTransferFromHost>);
 pub(super) type DrmIoctlVirtioGpuGetCaps        = ioc!(DRM_IOCTL_VIRTGPU_GET_CAPS,          b'd', 0x49, InOutData<VirtioGpuGetCaps>);
 pub(super) type DrmIoctlVirtioGpuMap            = ioc!(DRM_IOCTL_VIRTGPU_MAP,               b'd', 0x47, InOutData<VirtioGpuMap>);
 pub(super) type DrmIoctlVirtioGpuResourceInfo   = ioc!(DRM_IOCTL_VIRTGPU_RESOURCE_INFO,    b'd', 0x45, InOutData<VirtioGpuResourceInfo>);
