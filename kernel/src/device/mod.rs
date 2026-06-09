feaf380ebb73360f4772f85b74afcb4a92ccd512
Linermao <LinermaoGemail@gmail.com>
2026-06-09 14:30:36 +0800

    Add DRM file support and basic ioctls

--- kernel/src/device/mod.rs
+++ kernel/src/device/mod.rs
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: MPL-2.0
 
+mod drm;
 mod evdev;
 mod fb;
 mod mem;
@@ -166,7 +167,10 @@ pub fn init_in_first_kthread() {
     mem::init_in_first_kthread();
     misc::init_in_first_kthread();
     evdev::init_in_first_kthread();
-    fb::init_in_first_kthread();
+    if drm::init_in_first_kthread().is_err() {
+        // TODO: Remove this fallback once fbdev is provided through DRM emulation.
+        fb::init_in_first_kthread();
+    }
 }
 
 /// Initializes the device nodes in devtmpfs after mounting rootfs.
