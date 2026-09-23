133eb9e4cb9d52ad5e4bcae4701f850e1ad613d6
Linermao <LinermaoGemail@gmail.com>
2026-09-23 03:17:06 +0000

    Implement DRM dumb-buffer regression tests

---
+++ test/initramfs/src/regression/device/drm/gem_create_dumb_buffer.c
@@ -0,0 +1,137 @@
+// SPDX-License-Identifier: MPL-2.0
+
+#include <sys/mman.h>
+
+#include "../../common/test.h"
+#include "common.h"
+
+enum {
+	DUMB_WIDTH = 13,
+	DUMB_HEIGHT = 257,
+	DUMB_BPP = 15,
+};
+
+static int card_fd = -1;
+
+static int validate_created_buffer(const struct drm_mode_create_dumb *create,
+				   size_t page_size)
+{
+	uint64_t min_pitch =
+		((uint64_t)create->width * create->bpp + 7) / 8;
+	uint64_t min_size = min_pitch * create->height;
+	uint64_t expected_size =
+		(min_size + page_size - 1) / page_size * page_size;
+
+	return create->handle != 0 && create->pitch == min_pitch &&
+		       create->size == expected_size && create->size <= SIZE_MAX
+		       ? 0
+		       : -1;
+}
+
+static int validate_mmap_offset(const struct drm_mode_map_dumb *map,
+				size_t page_size)
+{
+	return map->offset != 0 && map->offset % page_size == 0 ? 0 : -1;
+}
+
+static int verify_zeroed_buffer(const uint8_t *buffer, size_t size)
+{
+	for (size_t i = 0; i < size; i++) {
+		if (buffer[i] != 0) {
+			return -1;
+		}
+	}
+
+	return 0;
+}
+
+static int write_and_verify_pattern(uint8_t *buffer, size_t size)
+{
+	for (size_t i = 0; i < size; i++) {
+		buffer[i] = (uint8_t)(i * 37 + 11);
+	}
+
+	for (size_t i = 0; i < size; i++) {
+		if (buffer[i] != (uint8_t)(i * 37 + 11)) {
+			return -1;
+		}
+	}
+
+	return 0;
+}
+
+FN_SETUP(open_card)
+{
+	card_fd = open_drm_node_or_skip(DRM_CARD_DEVICE);
+}
+END_SETUP()
+
+FN_TEST(create_map_destroy)
+{
+	long page_size = TEST_RES(sysconf(_SC_PAGESIZE), _ret > 0);
+
+	if (page_size > 0) {
+		struct drm_mode_create_dumb create = {
+			.width = DUMB_WIDTH,
+			.height = DUMB_HEIGHT,
+			.bpp = DUMB_BPP,
+		};
+		int create_result = TEST_RES(
+			ioctl(card_fd, DRM_IOCTL_MODE_CREATE_DUMB, &create),
+			_ret == 0);
+
+		if (create_result == 0) {
+			int validation_result = TEST_RES(
+				validate_created_buffer(&create, (size_t)page_size),
+				_ret == 0);
+
+			if (validation_result == 0) {
+				struct drm_mode_map_dumb map = {
+					.handle = create.handle,
+				};
+				int map_result = TEST_RES(
+					ioctl(card_fd, DRM_IOCTL_MODE_MAP_DUMB,
+					      &map),
+					_ret == 0);
+
+				if (map_result == 0 &&
+				    TEST_RES(validate_mmap_offset(
+						     &map, (size_t)page_size),
+					     _ret == 0) == 0) {
+					size_t map_size = (size_t)create.size;
+					uint8_t *buffer = TEST_RES(
+						mmap(NULL, map_size,
+						     PROT_READ | PROT_WRITE,
+						     MAP_SHARED, card_fd,
+						     (off_t)map.offset),
+						_ret != MAP_FAILED);
+
+					if (buffer != MAP_FAILED) {
+						TEST_RES(verify_zeroed_buffer(
+								 buffer, map_size),
+							 _ret == 0);
+						TEST_RES(write_and_verify_pattern(
+								 buffer, map_size),
+							 _ret == 0);
+						TEST_RES(munmap(buffer, map_size),
+							 _ret == 0);
+					}
+				}
+			}
+
+			struct drm_mode_destroy_dumb destroy = {
+				.handle = create.handle,
+			};
+			TEST_RES(ioctl(card_fd, DRM_IOCTL_MODE_DESTROY_DUMB,
+				       &destroy),
+				 _ret == 0);
+		}
+	}
+}
+END_TEST()
+
+FN_SETUP(close_card)
+{
+	CHECK(close(card_fd));
+}
+END_SETUP()
--- test/initramfs/src/regression/device/run_test.sh
+++ test/initramfs/src/regression/device/run_test.sh
@@ -5,6 +5,7 @@
 set -e
 
 ./drm/device_node
+./drm/gem_create_dumb_buffer
 ./drm/get_info
 ./drm/kms_resources
 ./drm/kms_properties
